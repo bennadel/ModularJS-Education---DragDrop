90fc5c2407f6c42a8f02e8d6fa8e736cfb67c2f0
Ben Nadel <ben@bennadel.com>
2011-12-23 10:13:23 -0500

    Removed the unshift() shim from the main file.

--- js/main.js
+++ js/main.js
@@ -1,31 +1,4 @@
 
-// Small prototype hack just to get unshift working in IE.
-if (!Array.prototype.hasOwnProperty( "unshift2" )){
-
-	// Define the unshift shim.
-	Array.prototype.unshift2 = function(){
-		
-		// Create a set of arguments to be used in the underlying
-		// splice() method call.
-		var spliceArguments = Array.prototype.slice.call( arguments );
-		
-		// Add the zero for index and delete count. This will convert
-		// the unshift() arguments collection into a collection that
-		// can be used with spliec();
-		spliceArguments.splice( 0, 0, 0, 0 );
-		
-		// Splice the unshifted elements on the array.
-		Array.prototype.splice.apply( this, spliceArguments );
-	
-	};
-
-}
-
-
-// ----------------------------------------------------------- //
-// ----------------------------------------------------------- //
-
-
 // Define the paths to be used in the script mappings. Also, define
 // the named module for certain libraries that are AMD compliant.
 require.config({
